61a0845ba316a22c837d9ce9b6c145156ab81550
avoymas01@gmail.com <avoymas01@gmail.com>
2025-09-09 00:00:34 -0400

    Add foundataions of full stack final project

--- FoundationsFullStack/FinalProject/FinalProject/obj/Debug/net9.0/FinalProject.AssemblyInfo.cs
+++ FoundationsFullStack/FinalProject/FinalProject/obj/Debug/net9.0/FinalProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+874abfe227153dca5dc63b12604b20ae577ce6cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
